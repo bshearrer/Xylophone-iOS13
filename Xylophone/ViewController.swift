d716c5e811418a4a97776be8a14f4547844bc4eb
bshearrer <bshearr@outlook.com>
2022-03-04 15:24:19 -0600

    Final Product

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+//Import for audio functionality
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    //audioPlayer allows us to play/pause our audio
+    var audioPlayer = AVAudioPlayer()
+    var audioSession = AVAudioSession.sharedInstance()
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        try! audioSession.setCategory(AVAudioSession.Category.ambient)
+        try! audioSession.setActive(true)
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        //Play button sound
+        playSound(soundName: sender.currentTitle!)
+        
+        //reduce button opacity to 50%
+        sender.alpha = 0.5
 
+        //Code should execute after a 0.2 second delay
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(200)) {
+            //button set to full opacity
+            sender.alpha = 1.0
+        }
+
+
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        audioPlayer = try! AVAudioPlayer(contentsOf: url!, fileTypeHint: "wav")
+        audioPlayer.play()
+    }
 }
 
